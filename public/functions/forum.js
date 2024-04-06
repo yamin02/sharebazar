fcef37fccc394ef0f59896b27d1b4c2c44f0a358
yamin02 <yamin136635@gmail.com>
2024-04-06 18:01:17 +0600

    updated

--- public/functions/forum.js
+++ public/functions/forum.js
@@ -30,7 +30,37 @@ module.exports.forum =  {
           <i class="fa fa-bookmark-o"> 12</i>
           <i class="fa fa-external-link"> 12</i>
       </div>
-      </div>`)          
+      </div>
+      
+
+
+      <div class="tweet">
+       <div class="tweet-header">
+          <div class="profile-info">
+              <img src="https://yaminulhoque.web.app/mypic.png" alt="Profile Picture">
+              <div>
+                  <h3>Yaminul Hoque</h3> 
+                  <span>@yamx02</span>
+              </div>
+          </div>
+          <button class="follow-button"># Featured </button>
+      </div>
+      <br>
+      <p>This is a tweet! 🔥</p>
+      <br>
+      <div class="tweet-image">
+          <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/45/Agriculture_of_Bangladesh_11.jpg/220px-Agriculture_of_Bangladesh_11.jpg" alt="Sample Image" class="tweet-image">
+      </div>
+      <br>
+      <div class="tweet-interactions">
+          <i class="fa fa-heart-o" > 12</i>
+          <i class="fa fa-comment-o" > 12</i>
+          <i class="fa fa-bookmark-o"> 12</i>
+          <i class="fa fa-external-link"> 12</i>
+      </div>
+      </div>
+      
+      `)          
     },
 
     repeatRend : async function () {  },
