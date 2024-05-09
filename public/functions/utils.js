b06824ee73448c0eb69d40c87b05a0518853f0c6
yamin02 <yamin136635@gmail.com>
2024-05-09 10:46:22 +0600

    changed some color

--- public/functions/utils.js
+++ public/functions/utils.js
@@ -24,7 +24,7 @@ module.exports.hideloading = () =>{
 
 module.exports.marketStatus = async () =>{
     var status = await api.dsex();
-    [p1,p2,p3] = (status['marketStatus'].toUpperCase() == "CLOSED") ? ["Closed","far fa-times-circle","rgb(96, 95, 93)"] : [`${status['marketStatus']}`,"far fa-check-circle", "#175500"]
+    [p1,p2,p3] = (status['marketStatus'].toUpperCase() == "CLOSED") ? ["Closed","far fa-times-circle","#F4BC1C"] : [`${status['marketStatus']}`,"far fa-check-circle", "#36bd04"]
     var p4 = (status["dsexChange"] > 0) ? 'green' : 'red' ;
     [dsexValue,dsexchange, dsexchangeP] = [status["dsex"].toFixed(3),status["dsexChange"].toFixed(3),status["dsexChangeP"].toFixed(2)] ;
     $("#dsex-info-navbar")
@@ -33,7 +33,7 @@ module.exports.marketStatus = async () =>{
     $("#marketstatus")
     .html(`
         <i class="${p2}"></i>
-        <i id="status001"><br>Market<br>${p1}</i>`)
+        <i id="status001"><br>Bank<br>${p1}</i>`)
     .css('color',`${p3}`)
     return p1;
 }
